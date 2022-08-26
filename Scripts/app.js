1e7f4ce8a5488d74136ec16cef54fc637dd718a4
Agent Ebimene <ebimeneagent@gmail.com>
2022-08-26 14:51:27 +0100

    Excellent Bridge Home Page

---
+++ Scripts/app.js
@@ -0,0 +1,52 @@
+const menuBar = document.querySelector(".menu-bars");
+const sideMenu = document.querySelector(".side-menu");
+const sideMenuList = document.querySelector(".side-menu-list");
+const closBtn = document.getElementById("close-btn");
+const companyLogo = document.querySelector(".company-logo");
+const toolList = document.getElementById;
+menuBar.addEventListener("click", () => {
+  sideMenu.classList.add("visible");
+  sideMenuList.style.minHeight = "300px";
+});
+closBtn.addEventListener("click", () => {
+  sideMenu.classList.remove("visible");
+});
+companyLogo.addEventListener("click", () => {
+  sideMenu.classList.add("visible");
+});
+alert("Hey");
+
+// // var slider = tns({
+// //   container: ".testimony-section",
+// //   items: 3,
+// //   gutter: 20,
+// //   slideBy: 1,
+// //   controlsPosition: "bottom",
+// //   navPosition: "bottom",
+// //   mouseDrag: true,
+// //   autoplay: true,
+// //   autoplayButtonOutput: false,
+// //   controlsContainer: "#custom-control",
+// //   responsive: {
+// //     0: {
+// //       items: 1,
+// //       nav: false,
+// //     },
+// //     768: {
+// //       items: 2,
+// //       nav: true,
+// //     },
+// //     1440: {
+// //       items: 3,
+// //     },
+// //   },
+// // mode: 'gallery',
+// // speed: 2000,
+// // animateIn: "scale",
+// // controls: false,
+// // nav: false,
+// // edgePadding: 20,
+// // loop: false,
+// // });
+
+// tns();
